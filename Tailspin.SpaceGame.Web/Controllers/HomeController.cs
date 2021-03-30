beb346e7b12a8a6674b447552bb3eb6921fb0d81
Rudra Kumar <rkumar@ccoding.onmicrosoft.com>
2021-03-29 20:45:04 -0400

    Update HomeController.cs

--- Tailspin.SpaceGame.Web/Controllers/HomeController.cs
+++ Tailspin.SpaceGame.Web/Controllers/HomeController.cs
@@ -44,7 +44,8 @@ namespace TailSpin.SpaceGame.Web.Controllers
                 {
                     "Solo",
                     "Duo",
-                    "Trio"
+                    "Trio",
+                    "Hello"
                 },
 
                     GameRegions = new List<string>()
